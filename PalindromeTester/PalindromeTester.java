d756689578ce91bc9dc380f91c5688d060456601
gcschmit <gcschmit@gmail.com>
2016-03-08 15:14:23 -0600

    Refactored PalindromeTester to be recursive

--- PalindromeTester/PalindromeTester.java
+++ PalindromeTester/PalindromeTester.java
@@ -11,36 +11,45 @@ public class PalindromeTester
      * Tests strings to see if they are palindromes.
      *
      */
-   public static void main (String[] args)
-   {
-      String str, another = "y";
-      int left, right;
-      Scanner s = new Scanner(System.in);
+    public static void main (String[] args)
+    {
+        String str, another = "y";
+        int left, right;
+        Scanner s = new Scanner(System.in);
 
-      while (another.equalsIgnoreCase("y")) // allows y or Y
-      {
-         System.out.println ("Enter a potential palindrome:");
-         str = s.nextLine();
+        while (another.equalsIgnoreCase("y")) // allows y or Y
+        {
+            System.out.println ("Enter a potential palindrome:");
+            str = s.nextLine();
 
-         left = 0;
-         right = str.length() - 1;
+            if( isPalindrome( str ) == true )
+            {
+                System.out.println ("That string IS a palindrome.");
+            }
+            else
+            {
+                System.out.println ("That string is NOT a palindrome.");
+            }
+            
+            System.out.println();
+            System.out.print ("Test another palindrome (y/n)? ");
+            another = s.nextLine();
+        }
+    }
 
-         while (str.charAt(left) == str.charAt(right) && left < right)
-         {
-            left++;
-            right--;
-         }
+    public static boolean isPalindrome( String str )
+    {
+        if( str.length() <= 1 )
+        {
+            return true;
+        }
+        
+        String substr = str.substring( 1, str.length() - 1 );
+        
+        return (( str.charAt( 0 ) == str.charAt( str.length() - 1 )) && isPalindrome( substr ));
+    }
 
-         System.out.println();
-
-         if (left < right)
-            System.out.println ("That string is NOT a palindrome.");
-         else
-            System.out.println ("That string IS a palindrome.");
-
-         System.out.println();
-         System.out.print ("Test another palindrome (y/n)? ");
-         another = s.nextLine();
-      }
-   }
 }
+
+
+
