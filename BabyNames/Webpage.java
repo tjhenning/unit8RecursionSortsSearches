c2f1337a9118997a212ca5909af09d6a2b8609e6
tjhenning <TJHenning@student.naperville203.org>
2016-03-23 15:08:07 -0500

    uuughhh

--- BabyNames/Webpage.java
+++ BabyNames/Webpage.java
@@ -9,17 +9,25 @@ public class Webpage
     {
         Scanner in = new Scanner(System.in);
         System.out.println("Give webpage URL.");
-        //https://www.google.com/?safe=active&ssui=on
-        //https://libraryofbabel.info/book.cgi?0-w1-s4-v17:1          
+        //http://www.google.com/?gws_rd=ssl&safe=active&ssui=on
+        //http://libraryofbabel.info/book.cgi?0-w1-s4-v17:1   
+        //http://en.wikipedia.org/wiki/Randomness (doesn't work)
+        //http://docs.oracle.com/javase/7/docs/api/java/awt/Shape.html
         URL page=new URL(in.next()); 
         InputStream wut=page.openStream();
-        in = new Scanner(wut);             
-        
-        
-//         
-//         PrintWriter out=new PrintWriter("output.txt");                                  
-//         out.println("u borked it");
-//         
-//         out.close();
+        in = new Scanner(wut);
+        PrintWriter out=new PrintWriter("output.txt");
+        while (in.hasNext())
+        {
+            out.println(in.nextLine());
+        }
+        in=new Scanner(page.openStream());
+        in.useDelimiter("<li><a href=");
+        while (in.hasNext())
+        {
+            System.out.println(in.nextLine());            
+        }
+         System.out.println("Done");
+        out.close();
     }
 }
